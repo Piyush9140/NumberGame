651863819a36f6d83d3fbd7d0e3c569a7184c306
Khushi740 <99756558+Piyush9140@users.noreply.github.com>
2024-01-14 02:54:44 +0530

    CustomButton UI changed

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+function pressHAndler() {
+  console.log("Pressed");
+}
 function PrimaryButton({ children }) {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={styles.buttonContainer}
+        onPress={pressHAndler}
+        android_ripple={{ color: "black" }}
+      >
+        <Text style={styles.buttontext}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonContainer: {
+   
+    backgroundColor: "#72063c",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    elevation: 2,
+  },
+  buttontext: {
+    color: "white",
+    textAlign: "center",
+  },
+});
